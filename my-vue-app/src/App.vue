ef15ec7d2e0698502ec1dba80e2dfc599837b0a9
amclean37 <Avery.McLean@gtri@gatech.edu>
2024-06-20 14:31:31 -0400

    reaction time

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <blockButton v-if="isPlaying" :delay="delay"/>
+  </div>
+ 
+</template>
+
+<script>
+import blockButton from './components/blockButton.vue'
+export default {
+  name: 'App',
+  components: { blockButton }, 
+
+  data(){
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app/src/components/blockButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+
+  data (){
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+
+  methods: {
+    // this function stores time data and run every 10 milisecs, and add 10 milisecs to reaction time
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer(){
+      clearInterval(this.timer)
+
+    }
+  }
+
+}
+</script>
+
+<style>
+    .block {
+        width:400px;
+        border-radius: 20px;
+        background: lightblue; 
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
